32251ccb544277df55c8ea9dbc1257a2f6e95561
cloudcoder88 <btech4104@gmail.com>
2025-08-03 18:23:02 +0100

    Moved from plain HTML to Angular components.
     Added property data in AppComponent.
    
     Used *ngFor to render listings dynamically.
    
     Fixed the details property error.
    <router-outlet>
     Imported CommonModule to support NgFor.
    
     Set up routing and cleaned index.html.
    added the description in the app.components structure
    Converted full HTML site into Angular component structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,42 +16,48 @@ export class AppComponent {
        location: 'Lekki',
        beds: 4,
        baths: 4,
-       image:'https://pictures-nigeria.jijistatic.net/176111110_MTIwMC0xNjAwLTNhOGU5YmQ2ZmY.webp'
+       image:'https://pictures-nigeria.jijistatic.net/176111110_MTIwMC0xNjAwLTNhOGU5YmQ2ZmY.webp',
+       description:' spacious kitchen,garden and secure parking,  close to hospitals,schools and major transport routes,active CCTV cameras and fire alarms' ,
     },
     { name: 'Ikeja Duplex',
       price: '95,000,000',
       location: 'Ikeja',
       beds: 3,
       baths: 3,
-      image:'https://pictures-nigeria.jijistatic.net/178962767_MTIwMC0xNjAwLTE5MzQxZGUwZmE.webp'
+      image:'https://pictures-nigeria.jijistatic.net/178962767_MTIwMC0xNjAwLTE5MzQxZGUwZmE.webp',
+      description:'24 hour electricity,fully furnished,located in a secure serene environment'
     },
     { name: 'Ajah Bungalow',
       price: '65,000,000',
       location: 'Ajah',
       beds: 2,
       baths: 2,
-      image:'https://pictures-nigeria.jijistatic.net/164240528_MTYwMC0yMTMzLWRhMWQ4NmE3ZjM.webp'
+      image:'https://pictures-nigeria.jijistatic.net/164240528_MTYwMC0yMTMzLWRhMWQ4NmE3ZjM.webp',
+      description:' 24-hour electricity with a backup generator, fully furnished,located in a secure and serene environment,'
     },
     { name: 'Ikeja Lodge',
       price: '1,000,000',
       location: 'Ikeja',
       beds: 1,
       baths: 1,
-      image:'https://cf.bstatic.com/xdata/images/hotel/max1024x768/645642762.jpg?k=46bec7c1caa1d746c339f5a1171247a427e636b97edb5e2fd9fbcd62067c4118&o'
+      image:'https://cf.bstatic.com/xdata/images/hotel/max1024x768/645642762.jpg?k=46bec7c1caa1d746c339f5a1171247a427e636b97edb5e2fd9fbcd62067c4118&o',
+      description:'24-hour electricity with a backup generator,Free on-site parking,free wifi,Air conditioning'
     },
     { name: '5bdrm Bungalow in Ajah Lekki',
       price: '120,000,000',
       location: 'Ajah Lekki',
       beds: 5,
       baths: 6,
-      image:'https://pictures-nigeria.jijistatic.net/168033895_MTA4MC0xMzUwLTQyMjUwMGQwNzA.webp'
+      image:'https://pictures-nigeria.jijistatic.net/168033895_MTA4MC0xMzUwLTQyMjUwMGQwNzA.webp',
+      description:'parking space,constant water supply  ,Fully furnished,located in a serene environment, 24-hour electricity'
     },
     { name: '5bdrm Bungalow in goshen estate ,Abuja',
       price: '120,000,000',
       location: 'goshen estate ,Abuja',
       beds: 5,
       baths: 6,
-      image:'https://pictures-nigeria.jijistatic.net/175019180_MTQ4OC0xNjAwLTgyMGRmMzRhOTk.webp'
+      image:'https://pictures-nigeria.jijistatic.net/175019180_MTQ4OC0xNjAwLTgyMGRmMzRhOTk.webp',
+      description:'fully furnished,located in a serene environment, reliable power supply with backup generator,constant water supply,parking space'
     }
   ];
   inquire(propertyName: string): void {
